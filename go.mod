554fdd625b86d51d67ddef1603d93dbd35f90951
fuqingrong <fuqingrong@dev01v.sj.corp.qihoo.net>
2021-08-02 21:53:03 +0800

    代码提交

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module xmlyd
 
 go 1.16
 
-require github.com/rikioy/xmlydownloader v0.0.0-20210802091720-e30dd8ca5430
+require (
+	github.com/cavaliercoder/grab v2.0.0+incompatible
+	github.com/gogf/gf v1.16.4
+	github.com/rikioy/xmlydownloader v0.0.0-20210802091720-e30dd8ca5430
+)
 
 replace github.com/rikioy/xmlydownloader v0.0.0-20210802091720-e30dd8ca5430 => /home/fuqingrong/develop/xmlydownloader
--- main.go
+++ main.go
@@ -3,33 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/cavaliercoder/grab"
+	"github.com/gogf/gf/os/gfile"
 	"github.com/rikioy/xmlydownloader"
 )
 
 func main() {
-	err := download(31909017)
+	err := download("jinbi1", 31909017)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func download(albumID int32) (err error) {
+func download(path string, albumID int32) (err error) {
 	info, err := xmlydownloader.GetAllAudioInfo(31909017)
 	//info, err := xmlydownloader.GetTrackList(31909017, 1, true)
 	if err != nil {
 		return
 	}
+	if !gfile.Exists(path) {
+		gfile.Mkdir(path)
+	}
 	for _, v := range info {
 		fmt.Printf("%d, %s, %t, %t\n", v.TrackID, v.Title, v.IsFree, v.IsPaid)
-		/*
-			tInfo, err := xmlydownloader.GetTrackInfoByMobile(v.TrackID, "")
+		if v.IsFree {
+			tInfo, err := xmlydownloader.GetFreeTrackInfo(v.TrackID, "")
 			if err != nil {
 				log.Fatalln(err)
 			}
-			grab.Get("./"+tInfo.Title+".mp3", tInfo.DownloadURL)
-			time.Sleep(5 * time.Second)
-		*/
+			filePath := fmt.Sprintf("./%s/%s.mp3", path, tInfo.Title)
+			fmt.Println(filePath)
+			grab.Get(filePath, tInfo.DownloadURL)
+			time.Sleep(2 * time.Second)
+		}
 	}
 	return
 }
